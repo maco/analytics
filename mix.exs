96147b96941db9b4131d2c413c25f09432ccdeec
Uku Taht <uku.taht@gmail.com>
2020-11-27 15:11:10 +0200

    WIP

--- mix.exs
+++ mix.exs
@@ -90,7 +90,9 @@ defmodule Plausible.MixProject do
       {:sshex, "2.2.1"},
       {:geolix, "~> 1.0"},
       {:clickhouse_ecto, git: "https://github.com/plausible/clickhouse_ecto.git"},
-      {:geolix_adapter_mmdb2, "~> 0.5.0"}
+      {:geolix_adapter_mmdb2, "~> 0.5.0"},
+      {:logflare_logger_backend, "~> 0.7.6"},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
   end
 
